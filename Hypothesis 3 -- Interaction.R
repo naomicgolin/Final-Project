3f74bc4bd48556f77d5922b9627c46b6e1fe1af9
Naomi Golin <ncgolin@gmail.com>
2022-02-27 16:19:33 -0800

    hypothesis 3 created

---
+++ Hypothesis 3 -- Interaction.R
@@ -0,0 +1,28 @@
+# Hypothesis 3: Time and Place
+library(readr)
+library(ggplot2)
+library(dplyr)
+library(lubridate)
+library(plotly)
+library(sf)
+library(maps)
+library(stringr)
+library(tidytext)
+library(tidyverse)
+library(rvest)
+library(httr)
+library(sf)
+library(raster)
+library(dplyr)
+library(spData)
+library(tmap)    # for static and interactive maps
+library(leaflet) # for interactive maps
+library(ggplot2) 
+library(grid)
+library(viridisLite)
+library(gifski)
+
+source(file="ufo_data.R") # Created in file named "Data Cleaning.R"
+source(file="state_population.R") # Created in file named "State Population df.R"
+
+
